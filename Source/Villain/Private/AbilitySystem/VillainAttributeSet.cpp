f5c5b59f7623b0730ea20d8b498887e4ca2f7ad9
bradley carrico <BCarrico89@gmail.com>
2024-09-23 12:22:18 -0700

    Attribute Set Setup

--- Source/Villain/Private/AbilitySystem/VillainAttributeSet.cpp
+++ Source/Villain/Private/AbilitySystem/VillainAttributeSet.cpp
@@ -2,4 +2,30 @@
 
 
 #include "AbilitySystem/VillainAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UVillainAttributeSet::UVillainAttributeSet()
+{
+	// TODO: Test Attribute Setting. Please Remove.
+	InitHealth(100.f);
+	InitMaxHealth(125.f);
+}
+
+void UVillainAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UVillainAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UVillainAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UVillainAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UVillainAttributeSet, Health, OldHealth);
+}
+
+void UVillainAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UVillainAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/Villain/Public/AbilitySystem/VillainAttributeSet.h
+++ Source/Villain/Public/AbilitySystem/VillainAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "VillainAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,22 @@ UCLASS()
 class VILLAIN_API UVillainAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UVillainAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// Vital Attributes
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UVillainAttributeSet, Health);
 	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UVillainAttributeSet, MaxHealth);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 };
